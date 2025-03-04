bcd6476658c0bbdc4c568cdb9473d8ef128668f7
harishanand51 <harishanand51@gmail.com>
2025-03-04 00:04:44 -0500

    first draft of portfolio

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { DiEclipse } from "react-icons/di";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiMacos,
+  SiWindows,
+  SiEclipseche,
+  SiEclipsejetty,
+  SiJira,
+  SiBitbucket,
+  SiMicrosoftoffice,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMacos />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiEclipse />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJira />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiBitbucket />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftoffice />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
